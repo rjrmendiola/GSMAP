46201bebe0169e93f1838c04d98c820a1bb6aec9
rjrmendiola <rogeliojr1234594@gmail.com>
2024-10-18 01:46:21 +0800

    Ongoing implementation of tabs in popup

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -76,7 +76,7 @@ export class LayoutComponent implements OnInit, AfterViewInit, OnDestroy {
     this.map = L.map('map', {
       center: [11.2966, 124.6783],
       zoom: 14,
-      zoomControl: true,
+      zoomControl: false,
       attributionControl: false
     }).setView([11.2977099, 124.6878707], 14);
 
@@ -254,6 +254,50 @@ export class LayoutComponent implements OnInit, AfterViewInit, OnDestroy {
   // Zoom to the feature on click
   private zoomToFeature(e: any) {
     this.map.fitBounds(e.target.getBounds()); // Fit the map to the bounds of the clicked feature
+
+    // image
+    const imageUrl =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Krakow_Center_-_basic_map.svg/1440px-Krakow_Center_-_basic_map.svg.png";
+
+    // new icon
+    const funny = L.icon({
+      iconUrl: "http://grzegorztomicki.pl/serwisy/pin.png",
+      iconSize: [50, 58], // size of the icon
+      iconAnchor: [20, 58], // changed marker icon position
+      popupAnchor: [0, -60], // changed popup position
+    });
+
+    // custom popup image + text
+    const customPopup =
+    '<div class="customPopup"><figure><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg/1920px-A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg"><figcaption>Source: wikipedia.org</figcaption></figure><div>Kraków,[a] also written in English as Krakow and traditionally known as Cracow, is the second-largest and one of the oldest cities in Poland. Situated on the Vistula River in Lesser Poland Voivodeship... <a href="https://en.wikipedia.org/wiki/Krak%C3%B3w" target="_blank">→ show more</a></div></div>';
+
+    // specify popup options
+    const customOptions = {
+      minWidth: 220, // set max-width
+      keepInView: true, // Set it to true if you want to prevent users from panning the popup off of the screen while it is open.
+    };
+
+    // create marker object, pass custom icon as option, pass content and options to popup, add to map
+    L.marker([11.2966, 124.6783], {
+      icon: funny,
+    })
+    .bindPopup(customPopup, customOptions)
+    .on("click", this.clickZoom)
+    .addTo(this.map);
+
+    // add image to map
+    const imageBounds: L.LatLngBoundsExpression = [
+      [11.2966, 124.6783],
+      [11.0966, 124.0783],
+    ];
+
+    // L.imageOverlay(imageUrl, imageBounds, { opacity: 1 }).addTo(this.map);
+  }
+
+  // center map when click on marker
+  private clickZoom(e: any) {
+    // this.map.setView(e.target.getLatLng(), zoom);
+    this.map.setView(e.target.getLatLng(), 14);
   }
 
   @ViewChild('map')
