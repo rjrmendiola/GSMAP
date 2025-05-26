be206083cb538033e2e886276a12c08bf2071807
rjrmendiola <rogeliojr1234594@gmail.com>
2025-05-26 11:25:30 +0800

    Fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./modules/landing-page/landing-page.module').then((m) => m.LandingPageModule),
-  // },
+  {
+    path: '',
+    loadChildren: () => import('./modules/landing-page/landing-page.module').then((m) => m.LandingPageModule),
+  },
   {
     path: 'layout',
     loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
@@ -14,17 +14,17 @@ const routes: Routes = [
   //   path: 'auth',
   //   loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   // },
-  // {
-  //   path: 'errors',
-  //   loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
-  // },
-  // { path: '**', redirectTo: 'errors/404' },
+  {
+    path: 'errors',
+    loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
+  },
   {
     path: 'admin',
     loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
     // canActivate: [AuthGuard],
     data: { role: 'admin' }
-  }
+  },
+  { path: '**', redirectTo: 'errors/404' }
 ];
 
 @NgModule({
