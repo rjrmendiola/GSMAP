c70048c1d4b615d9730f520d87b824415f00d3bf
Blas Mendiola <rbfmendiola@gmail.com>
2025-07-02 01:06:34 +0800

    Redirect landing page to admin dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./modules/landing-page/landing-page.module').then((m) => m.LandingPageModule),
+  // },
   {
+    // path: 'layout',
     path: '',
-    loadChildren: () => import('./modules/landing-page/landing-page.module').then((m) => m.LandingPageModule),
-  },
-  {
-    path: 'layout',
     loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
   },
   {
