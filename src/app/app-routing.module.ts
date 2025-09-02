f51c6a8142b6c9b7634345a0e0e95e75af7b7def
rjrmendiola <rogeliojr1234594@gmail.com>
2025-09-03 05:32:12 +0800

    Create separate modules for ordinary user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,23 +9,24 @@ const routes: Routes = [
   {
     // path: 'layout',
     path: '',
-    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+    loadChildren: () => import('./modules/user/user.module').then((m) => m.UserModule),
   },
-  {
-    path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
-    path: 'errors',
-    loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
-  },
-  {
-    path: 'admin',
-    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
-    // canActivate: [AuthGuard],
-    data: { role: 'admin' }
-  },
-  { path: '**', redirectTo: 'errors/404' }
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  // },
+  // {
+  //   path: 'errors',
+  //   loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule),
+  // },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
+  //   // canActivate: [AuthGuard],
+  //   data: { role: 'admin' }
+  // },
+  // { path: '**', redirectTo: 'errors/404' }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
