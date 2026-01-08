69b47211f964a0f3050c0c162458f32a5c69a779
rjrmendiola <rogeliojr1234594@gmail.com>
2026-01-08 23:54:20 +0800

    Hide User and Weather Settings menus

--- src/app/shared/config/sidebar-links.config.ts
+++ src/app/shared/config/sidebar-links.config.ts
@@ -9,8 +9,8 @@ export const ADMIN_LINKS = [
   { label: 'Barangays', path: '/admin/barangays', icon: 'people' },
   { label: 'Officials', path: '/admin/officials', icon: 'people' },
   { label: 'Evacuation Centers', path: '/admin/evacuation-centers', icon: 'people' },
-  { label: 'Users', path: '/admin/users', icon: 'admin_panel_settings' },
-  { label: 'Weather Settings', path: '/admin/weather-settings', icon: 'admin_panel_settings' },
+  // { label: 'Users', path: '/admin/users', icon: 'admin_panel_settings' },
+  // { label: 'Weather Settings', path: '/admin/weather-settings', icon: 'admin_panel_settings' },
   // { label: 'Manage Users', path: '/admin/users', icon: 'admin_panel_settings' },
   // Add other admin links
 ];
